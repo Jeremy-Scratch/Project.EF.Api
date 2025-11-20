4f27be294e4c8d32ce1bb475c3eb34c62a4f03ee
Scratch <Jeremyscratch6@gmail.com>
2025-11-20 01:34:57 -0400

    Add: Attributes mapping

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Project.EF.Api.Models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+    [Required]
+    [MaxLength(150)]
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public ICollection<Task>? Tasks { get; set; }
+    public ICollection<Tarea>? Tareas { get; set; }
 }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,15 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Project.EF.Api.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get; set; }
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string? Title { get; set; }
+    [MaxLength(200)]
     public string? Description { get; set; }
     public Priority TareaPriority { get; set; }
     public DateTime CreationDate { get; set; }
-    public virtual Category? Category { get; set; } 
-
+    public virtual Category? Category { get; set; }
+    [NotMapped] 
+    public string? Resume{ get; set; }
 }
 
 public enum Priority
