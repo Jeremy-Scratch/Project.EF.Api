9e65c049e80d8b9b5e0378e80a0dc4e4ff36a828
Scratch <Jeremyscratch6@gmail.com>
2025-11-20 02:11:39 -0400

    test: in memory data base

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.EF.Api.EFContext;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(x => x.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
