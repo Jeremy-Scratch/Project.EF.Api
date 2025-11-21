e48cda1cdf3107cb3e06a46ba875f4766a419139
Scratch <Jeremyscratch6@gmail.com>
2025-11-21 06:29:08 -0400

    add: modelBuilder - fluent API

--- EFContext/TareaContext.cs
+++ EFContext/TareaContext.cs
@@ -7,5 +7,25 @@ public class TareasContext: DbContext
     public DbSet<Category> Categories { get; set; }
 
     public DbSet<Tarea> Tareas { get; set; }
-    public TareasContext(DbContextOptions<TareasContext> options) : base(options){}
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.ToTable("Category");
+            category.HasKey(x => x.CategoryId);
+            category.Property(x => x.Name).IsRequired().HasMaxLength(150);
+            category.Property(x => x.Description).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<Tarea>(tarea =>
+        {
+            tarea.ToTable("Task");
+            tarea.HasKey(x => x.TareaId);
+            tarea.Property(x => x.Title).IsRequired().HasMaxLength(150);
+            tarea.Property(x => x.Description).HasMaxLength(200);
+            tarea.Ignore(x=>x.Resume);
+        });
+    }
 }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -5,14 +5,14 @@ namespace Project.EF.Api.Models;
 
 public class Tarea
 {
-    [Key]
+    //[Key]
     public Guid TareaId { get; set; }
     [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
-    [Required]
-    [MaxLength(200)]
+    //[Required]
+    //[MaxLength(200)]
     public string? Title { get; set; }
-    [MaxLength(200)]
+    //[MaxLength(200)]
     public string? Description { get; set; }
     public Priority TareaPriority { get; set; }
     public DateTime CreationDate { get; set; }
